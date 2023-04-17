8c74523a2e438bc46cd10eae8e663f701929d1e1
Harmonster <dennyharmless@gmail.com>
2023-04-17 18:16:07 +0700

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnFirst = new System.Windows.Forms.ToolStripButton();
+            this.btnPrevious = new System.Windows.Forms.ToolStripButton();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.btnLast = new System.Windows.Forms.ToolStripButton();
+            this.btnNext = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvData
+            // 
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvData.Location = new System.Drawing.Point(0, 0);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.Size = new System.Drawing.Size(800, 450);
+            this.dgvData.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnFirst,
+            this.btnPrevious,
+            this.toolStripLabel1,
+            this.btnNext,
+            this.btnLast,
+            this.toolStripSeparator1,
+            this.toolStripTextBox1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 425);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnFirst.Image = ((System.Drawing.Image)(resources.GetObject("btnFirst.Image")));
+            this.btnFirst.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(27, 22);
+            this.btnFirst.Text = "<<";
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnPrevious.Image = ((System.Drawing.Image)(resources.GetObject("btnPrevious.Image")));
+            this.btnPrevious.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(23, 22);
+            this.btnPrevious.Text = "<";
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(86, 22);
+            this.toolStripLabel1.Text = "toolStripLabel1";
+            // 
+            // btnLast
+            // 
+            this.btnLast.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnLast.Image = ((System.Drawing.Image)(resources.GetObject("btnLast.Image")));
+            this.btnLast.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(27, 22);
+            this.btnLast.Text = ">>";
+            // 
+            // btnNext
+            // 
+            this.btnNext.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnNext.Image = ((System.Drawing.Image)(resources.GetObject("btnNext.Image")));
+            this.btnNext.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(23, 22);
+            this.btnNext.Text = ">";
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 25);
+            this.toolStripTextBox1.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.dgvData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnFirst;
+        private System.Windows.Forms.ToolStripButton btnPrevious;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripButton btnNext;
+        private System.Windows.Forms.ToolStripButton btnLast;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,186 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        private int _currentPageIndex = 1;
+        private int _pageSize = 10;
+        private string _sortColumn = "№ запроса";
+        private string _sortDirection = "ASC";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            string searchText = toolStripTextBox1.Text.Trim();
+            DataTable dataTable = GetFilteredData(searchText, _currentPageIndex, _pageSize, _sortColumn, _sortDirection);
+            dgvData.DataSource = dataTable;
+            dgvData.Columns["RowNumber"].Visible = false;
+        }
+
+        private DataTable GetFilteredData(string searchText, int pageIndex, int pageSize, string sortColumn, string sortDirection)
+        {
+            // Retrieve data from the database
+            string connectionString = "server=37.77.105.162;user id=Tarabotti;password=gGJ130T90!;persistsecurityinfo=True;database=Diplom";
+            string query = @"	SELECT `id_ticket` AS `№ запроса`, `name_type` AS `Тип`, `name_status` AS `Статус`, `name_priority` AS `Приоритет`, `name_staff` AS `Автор`, `date_ticket` AS `Дата создания`, `misc_ticket` AS `Детали` FROM `Diplom`.`tickets`
+			JOIN `Diplom`.`ticket_type` ON `Diplom`.`tickets`.`type_ticket` = `Diplom`.`ticket_type`.`id_type`
+			JOIN `Diplom`.`ticket_status` ON `Diplom`.`tickets`.`status_ticket` = `Diplom`.`ticket_status`.`id_status`
+			JOIN `Diplom`.`ticket_priority` ON `Diplom`.`tickets`.`priority_ticket` = `Diplom`.`ticket_priority`.`id_priority`
+			JOIN `Diplom`.`staff` ON `Diplom`.`tickets`.`author_ticket` = `Diplom`.`staff`.`id_staff`
+		ORDER BY `id_ticket`;";
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                MySqlDataAdapter adapter = new MySqlDataAdapter(query, connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                // Add a RowNumber column to the DataTable
+                if (!dataTable.Columns.Contains("RowNumber"))
+                {
+                    dataTable.Columns.Add("RowNumber", typeof(int));
+                }
+
+                
+                // Populate the RowNumber column with the row numbers for each row
+                for (int i = 0; i < dataTable.Rows.Count; i++)
+                {
+                    dataTable.Rows[i]["RowNumber"] = i + 1;
+                }
+
+                // Filter the data based on the search text
+                if (!string.IsNullOrEmpty(searchText))
+                {
+                    string filterExpression = GetFilteredExpression();
+                    DataRow[] filteredRows = dataTable.Select(filterExpression);
+                    dataTable = filteredRows.Any() ? filteredRows.CopyToDataTable() : dataTable.Clone();
+                }
+
+                // Sort the data based on the sort column and direction
+                if (!string.IsNullOrEmpty(sortColumn) && !string.IsNullOrEmpty(sortDirection))
+                {
+                    string sortExpression = string.Format("{0} {1}", sortColumn, sortDirection);
+                    dataTable.DefaultView.Sort = sortExpression;
+                    dataTable = dataTable.DefaultView.ToTable();
+                }
+
+                // Calculate the start index and end index of the current page of data
+                int startIndex = (pageIndex - 1) * pageSize;
+                int endIndex = Math.Min(startIndex + pageSize, dataTable.Rows.Count) - 1;
+
+                // Create a new DataTable to hold the current page of data
+                DataTable pageDataTable = dataTable.Clone();
+
+                // Populate the new DataTable with the current page of data
+                for (int i = startIndex; i <= endIndex; i++)
+                {
+                    DataRow row = dataTable.Rows[i];
+                    pageDataTable.ImportRow(row);
+                }
+
+                // Calculate the total number of pages
+                int totalRows = dataTable.Rows.Count;
+                int totalPages = (int)Math.Ceiling((double)totalRows / pageSize);
+
+                // Update the pagination controls
+                UpdatePaginationControls(totalPages);
+
+                // Return the current page of data
+                return pageDataTable;
+            }
+        }
+
+        private string GetFilteredExpression()
+        {
+            string searchValue = toolStripTextBox1.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(searchValue))
+            {
+                return null;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            foreach (DataGridViewColumn column in dgvData.Columns)
+            {
+                if (column.Visible && column.ValueType == typeof(string))
+                {
+                    if (sb.Length > 0)
+                    {
+                        sb.Append(" OR ");
+                    }
+                    sb.AppendFormat("{0} LIKE '%{1}%'", column.DataPropertyName, searchValue);
+                }
+            }
+
+            if (sb.Length > 0)
+            {
+                return sb.ToString();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private void UpdatePaginationControls(int totalPages)
+        {
+            // Update the current page label
+            toolStripLabel1.Text = $"Page {_currentPageIndex} of {totalPages}";
+
+            // Enable/disable the previous button
+            btnPrevious.Enabled = (_currentPageIndex > 1);
+
+            // Enable/disable the next button
+            btnNext.Enabled = (_currentPageIndex < totalPages);
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            _currentPageIndex = 1;
+            LoadData();
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            _currentPageIndex--;
+            LoadData();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            _currentPageIndex++;
+            LoadData();
+        }
+
+        private void dgvData_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            // Determine the sort column and direction based on the column header clicked
+            string sortColumn = dgvData.Columns[e.ColumnIndex].DataPropertyName;
+            string sortDirection = (_sortColumn == sortColumn && _sortDirection == "ASC") ? "DESC" : "ASC";
+
+            // Update the sort column and direction
+            _sortColumn = sortColumn;
+            _sortDirection = sortDirection;
+
+            // Reload the data
+            LoadData();
+        }
+    }
+}
